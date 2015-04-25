3bef60c12506d281296720b5b2f924afed66e1c6
Spymannn <samir_hanini@live.be>
2015-04-25 13:44:11 +0200

    Added firework particles

--- src/factory.lua
+++ src/factory.lua
@@ -5,7 +5,8 @@ require 'component_auto_remove'
 
 Factory = Factory or {
     objects = {},
-    collisionParticles = {}
+    collisionParticles = {},
+    fireworkParticles = {}
 }
 
 function Factory:pickFromPool(t)
@@ -153,6 +154,109 @@ function Factory:createCollisionParticle()
             )
     end
 
+    e.particle:reset()
+
+    return e
+end
+
+function Factory:createFireworkParticle()
+    local e = self:pickFromPool(self.fireworkParticles)
+
+    if not e then
+        e = gengine.entity.create() 
+
+        e:addComponent(
+            ComponentParticleSystem(),
+            {
+                texture = gengine.graphics.texture.get("particle"),
+                size = 100,
+                emitterRate = 1000,
+                emitterLifeTime = 1,
+                extentRange = {vector2(32,32), vector2(34,34)},
+                lifeTimeRange = {0, 1},
+                directionRange = {0, 2*3.14},
+                speedRange = {500, 500},
+                rotationRange = {0, 0},
+                spinRange = {0, 1},
+                linearAccelerationRange = {vector2(0,-400), vector2(0,-500)},
+                scales = {vector2(0.2, 0.2)},
+                colors = {vector4(0.8,0.8,0.9,1), vector4(0.3,0.3,0.9,1), vector4(0,0,0,0)}
+            }
+            )
+
+         e:addComponent(
+            ComponentParticleSystem(),
+            {
+                texture = gengine.graphics.texture.get("particle"),
+                size = 100,
+                emitterRate = 1000,
+                emitterLifeTime = 1,
+                extentRange = {vector2(32,32), vector2(34,34)},
+                lifeTimeRange = {0, 1},
+                directionRange = {0, 2*3.14},
+                speedRange = {500, 500},
+                rotationRange = {0, 0},
+                spinRange = {0, 1},
+                linearAccelerationRange = {vector2(0,-400), vector2(0,-500)},
+                scales = {vector2(0.2, 0.2)},
+                colors = {vector4(0.3,0.8,0.4,1), vector4(0.3,0.6,0.6,1), vector4(0,0,0,0)}
+            }
+            )
+
+        e:addComponent(
+            ComponentParticleSystem(),
+            {
+                texture = gengine.graphics.texture.get("particle"),
+                size = 100,
+                emitterRate = 1000,
+                emitterLifeTime = 1,
+                extentRange = {vector2(32,32), vector2(34,34)},
+                lifeTimeRange = {0, 1},
+                directionRange = {0, 3.14},
+                speedRange = {500, 500},
+                rotationRange = {0, 0},
+                spinRange = {0, 1},
+                linearAccelerationRange = {vector2(0,-400), vector2(0,-500)},
+                scales = {vector2(0.2, 0.2)},
+                colors = {vector4(0.8,0.3,0.4,1), vector4(0.9,0.6,0.3,1), vector4(0,0,0,0)}
+            }
+            )
+
+
+         e:addComponent(
+            ComponentParticleSystem(),
+            {
+                texture = gengine.graphics.texture.get("particle"),
+                size = 100,
+                emitterRate = 1000,
+                emitterLifeTime = 1,
+                extentRange = {vector2(32,32), vector2(34,34)},
+                lifeTimeRange = {0, 1},
+                directionRange = {0, 3.14},
+                speedRange = {500, 500},
+                rotationRange = {0, 0},
+                spinRange = {0, 1},
+                linearAccelerationRange = {vector2(0,-400), vector2(0,-500)},
+                scales = {vector2(0.2, 0.2)},
+                colors = {vector4(0.8,0.1,0.1,1), vector4(1,1,0,1), vector4(0,0,0,0)}
+            }
+            )
+         e:addComponent(
+            ComponentPoolable(),
+            {
+                pool = self.fireworkParticles
+            }
+            )
+
+        e:addComponent(
+            ComponentAutoRemove(),
+            {
+               duration = 2
+            }
+            )
+
+        end
+    
     e.particle:reset()
 
     return e
